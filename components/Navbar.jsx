31178509658ee06b212310efc331c1e96d6a5006
Muhammad Amirxon <muhammadamirxon14@gmail.com>
2024-10-31 16:01:34 +0500

    SMA

---
+++ components/Navbar.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Navbar() {
+    return (
+        <nav className="h-[100px] bg-black text-white flex justify-between items-center p-[50px]">
+
+            <a href=""><h1 className="text-2xl">Logo</h1></a>
+
+            <button className="w-[150px] h-[50px] rounded-[10px] text-black bg-white">Btn</button>
+
+        </nav>
+    );
+  }
+  
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import "@/styles/globals.css";
+import Navbar from "@/components/Navbar";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  
+  const isAbout = router.pathname === '/Navbar';
+
+  return (
+    <div>
+      {!isAbout && <Navbar />}
+      <Component {...pageProps} />
+    </div>
+  );
 }
