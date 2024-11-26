516ce576f264350391284fc1325783620cdf0287
Muhammad Amirxon <muhammadamirxon14@gmail.com>
2024-11-26 14:47:48 +0500

    hi

--- pages/P1.jsx
+++ pages/P1.jsx
@@ -5,7 +5,136 @@ const Page1 =()=>{
   return (
     <div>
 
-    <div className="bg-[#C5010A] h-[400px] mt-[-100px]"><h1 className="text-white text-center text-5xl pt-[2  00px]">Kawasaki NINJA® 1100SX</h1></div>
+    <div className="bg-[#C5010A] h-[400px] mt-[-100px]"><h1 className="text-white text-center text-5xl pt-[200px]">Kawasaki NINJA® 1100SX</h1></div>
+
+
+
+
+
+
+
+    <h1 className="text-4xl pl-[30px] pt-[100px]">Home - Inventory</h1>
+
+    <div className="flex justify-center gap-[20px] pt-[100px]">
+
+    <div class="paste-button">
+              <button class="button1"><h2>Make</h2><h2>▼</h2></button>
+              <div class="dropdown-content">
+                <a id="top" href="#">Keep source formatting</a>
+                <a id="middle" href="#">Merge formatting</a>
+                <a id="bottom" href="#">Keep text only</a>
+              </div>
+            </div>
+
+            <div class="paste-button">
+              <button class="button1"><h2>Module</h2><h2>▼</h2></button>
+              <div class="dropdown-content">
+                <a id="top" href="#">Keep source formatting</a>
+                <a id="middle" href="#">Merge formatting</a>
+                <a id="bottom" href="#">Keep text only</a>
+              </div>
+            </div>
+
+            <div class="paste-button">
+              <button class="button1"><h2>Condition</h2><h2>▼</h2></button>
+              <div class="dropdown-content">
+                <a id="top" href="#">Keep source formatting</a>
+                <a id="middle" href="#">Merge formatting</a>
+                <a id="bottom" href="#">Keep text only</a>
+              </div>
+            </div>
+
+            <div class="paste-button">
+              <button class="button1"><h2>Transmission</h2><h2>▼</h2></button>
+              <div class="dropdown-content">
+                <a id="top" href="#">Keep source formatting</a>
+                <a id="middle" href="#">Merge formatting</a>
+                <a id="bottom" href="#">Keep text only</a>
+              </div>
+            </div>
+
+    </div>
+
+
+    <div className="flex justify-center gap-[20px] pt-[20px]">
+
+<div class="paste-button">
+          <button class="button1"><h2>Body</h2><h2>▼</h2></button>
+          <div class="dropdown-content">
+            <a id="top" href="#">Keep source formatting</a>
+            <a id="middle" href="#">Merge formatting</a>
+            <a id="bottom" href="#">Keep text only</a>
+          </div>
+        </div>
+
+        <div class="paste-button">
+          <button class="button1"><h2>Drive</h2><h2>▼</h2></button>
+          <div class="dropdown-content">
+            <a id="top" href="#">Keep source formatting</a>
+            <a id="middle" href="#">Merge formatting</a>
+            <a id="bottom" href="#">Keep text only</a>
+          </div>
+        </div>
+
+        <div class="paste-button">
+          <button class="button1"><h2>Fule Type</h2><h2>▼</h2></button>
+          <div class="dropdown-content">
+            <a id="top" href="#">Keep source formatting</a>
+            <a id="middle" href="#">Merge formatting</a>
+            <a id="bottom" href="#">Keep text only</a>
+          </div>
+        </div>
+
+        <div class="paste-button">
+          <button class="button1"><h2>Color</h2><h2>▼</h2></button>
+          <div class="dropdown-content">
+            <a id="top" href="#">Keep source formatting</a>
+            <a id="middle" href="#">Merge formatting</a>
+            <a id="bottom" href="#">Keep text only</a>
+          </div>
+        </div>
+
+</div>
+
+
+
+
+<div className="flex justify-around">
+
+  <div className="flex justify-center gap-[20px]">
+
+  </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     <div className="pt-[100px] bg-gray-100 h-[500px]">
 
